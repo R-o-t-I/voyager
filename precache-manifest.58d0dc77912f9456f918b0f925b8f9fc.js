577eb12b2e24e96c4acf80472265d97bd8525577
R-o-t-I <rulers.oftheinfinite@mail.ru>
2022-06-05 04:04:58 +0300

    Updates

---
+++ precache-manifest.58d0dc77912f9456f918b0f925b8f9fc.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1dad1d4a0d27888a26c",
+    "url": "/voyager/static/css/main.2e6b9191.chunk.css"
+  },
+  {
+    "revision": "f1dad1d4a0d27888a26c",
+    "url": "/voyager/static/js/main.e652952e.chunk.js"
+  },
+  {
+    "revision": "9526c5ab9678552d0480",
+    "url": "/voyager/static/js/runtime~main.9ff62401.js"
+  },
+  {
+    "revision": "4102cb09a7df93771b54",
+    "url": "/voyager/static/css/2.65c7f6b1.chunk.css"
+  },
+  {
+    "revision": "4102cb09a7df93771b54",
+    "url": "/voyager/static/js/2.cde8e150.chunk.js"
+  },
+  {
+    "revision": "af699a9c6e9a7d6587af",
+    "url": "/voyager/static/css/3.e9d5b617.chunk.css"
+  },
+  {
+    "revision": "af699a9c6e9a7d6587af",
+    "url": "/voyager/static/js/3.238a9728.chunk.js"
+  },
+  {
+    "revision": "4a58be5251bfd09a6b37",
+    "url": "/voyager/static/css/4.4fe1714c.chunk.css"
+  },
+  {
+    "revision": "4a58be5251bfd09a6b37",
+    "url": "/voyager/static/js/4.a4716269.chunk.js"
+  },
+  {
+    "revision": "d770e99b78a2d47f8546",
+    "url": "/voyager/static/css/5.0e433876.chunk.css"
+  },
+  {
+    "revision": "d770e99b78a2d47f8546",
+    "url": "/voyager/static/js/5.bd40195b.chunk.js"
+  },
+  {
+    "revision": "7708429d13494d4cd7c4",
+    "url": "/voyager/static/js/6.54eb0419.chunk.js"
+  },
+  {
+    "revision": "f7fc790fd58e56507357",
+    "url": "/voyager/static/js/7.de620926.chunk.js"
+  },
+  {
+    "revision": "413a388fa0baa7355bb3",
+    "url": "/voyager/static/js/8.45c7d1a1.chunk.js"
+  },
+  {
+    "revision": "f8edf62f336dbcbdcb0d1c28930b0b28",
+    "url": "/voyager/static/media/restaurant.f8edf62f.svg"
+  },
+  {
+    "revision": "8ec255faf8aaa411819d6d7474c33544",
+    "url": "/voyager/static/media/culturalHeritage.8ec255fa.svg"
+  },
+  {
+    "revision": "854fe44086df2f40a4589e4a58b70689",
+    "url": "/voyager/static/media/religion.854fe440.svg"
+  },
+  {
+    "revision": "727409bde41078f6dc6c4f8fd31b023d",
+    "url": "/voyager/index.html"
+  }
+];
